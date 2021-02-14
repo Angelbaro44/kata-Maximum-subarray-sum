7f195991f41ffa8795fb1774f414a61656dbdb5b
Angel Baro <angelbaro44@gmail.com>
2021-02-13 20:27:04 -0500

    Added test cases

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ var maxSequence = function(arr){
         }
         return maxNum
     }
+
+
+
+
+// Test Cases
+
+
+console.log(maxSequence([])); // Expected Result: 0 
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Expected Result: 6 
+console.log(maxSequence([-2, -1, -3, -4, -1, -2, -1, -5, -4])); // Expected Result: 0 
+console.log(maxSequence([ 7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43 ])); // Expected Result: 155);
