c3ab970dc2eea0efce50f0b6f835a69703441694
Angel Baro <angelbaro44@gmail.com>
2021-02-13 20:26:03 -0500

    Added notes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+//First load a var with a sum of numbers from the list. When it reaches a peck value 
+//it gets recorded to a secound var with the highest subarr total. Now lastly, if 
+// the sum dips into the negitave the sum var resets allowing a search of the rest of the list
+// perserving the subarr total.
+// EX: maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+//                            [4, -1, 2, 1] (sum = 6)
+
+
+
 var maxSequence = function(arr){
       let sumStream=0;
       let maxNum=0;
